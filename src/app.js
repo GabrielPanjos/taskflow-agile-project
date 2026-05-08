490f113329ee5a7689d0c68e15fc7b5656569659
GabrielPanjos <pereiradosanjosgabriel@gmail.com>
2026-05-07 21:06:06 -0300

    feat: adiciona exclusão de tarefas

--- src/app.js
+++ src/app.js
@@ -8,12 +8,13 @@ function createTask(title) {
   return {
     id: Date.now(),
     title,
-    done: false
+    done: false,
   };
 }
 
 function addTask() {
   const input = document.getElementById("taskInput");
+
   const task = createTask(input.value);
 
   if (!task) {
@@ -22,7 +23,36 @@ function addTask() {
   }
 
   tasks.push(task);
+
   renderTasks();
 
   input.value = "";
+}
+
+function renderTasks() {
+  const taskList = document.getElementById("taskList");
+
+  taskList.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      <span>${task.title}</span>
+      <button onclick="deleteTask(${task.id})">
+        ❌
+      </button>
+    `;
+
+    taskList.appendChild(li);
+  });
+}
+
+function deleteTask(id) {
+  const index = tasks.findIndex((task) => task.id === id);
+
+  if (index !== -1) {
+    tasks.splice(index, 1);
+    renderTasks();
+  }
 }
